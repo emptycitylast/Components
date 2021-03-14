764a88de9ca9dbdbe97116ca8172aa4b393efc4e
peking <emptycity2578@aliyun.com>
2021-03-15 00:27:31 +0800

    Update seed.lua

--- lua/seed.lua
+++ lua/seed.lua
@@ -18,26 +18,24 @@ function seed(key, digit)
         self.key = valueToRedo
         valueToRedo = ''
     end
-
+   --防止开头有0
     while (string.len(key) % digit ~= 0) do
         key = 0 .. key
     end
-
+    --字符转为数字
     for i = 1, string.len(key) do
         keyToRedo = keyToRedo .. string.sub(key, i, i):byte() * i * 9 % 10
     end
     while (string.len(keyToRedo) % digit ~= 0) do
         keyToRedo = keyToRedo .. (string.len(keyToRedo) % digit * 7) % 10
     end
-
+    --将位数转为规定位数
     local multiple = string.len(keyToRedo) % digit
     for i = 1, digit do
         valueToRedo = valueToRedo .. string.sub(keyToRedo, i + multiple, i + multiple)
     end
-
+    --转换为value
     value = string.reverse(valueToRedo)
-
     self.value = value
     return self
 end
-print(seed('时间计算机', 10).key, seed('时间计算机', 10).value)
